9e81fbcfda3dd63f3c48c05349a8ca4fb378c734
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-11-24 22:10:29 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express')
 const path = require('path')
 const stocks = require('./stocks')
+const cors = require('cors');
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'static')))
-
-app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+app.use(cors({ origin: '*' })); // to fix the CORS error
+/* Get the list of stocks and catch the error */
+app.get('/stocks', async(req, res) => {
+    try {
+        const stockSymbols = await stocks.getStocks()
+        res.json({ stockSymbols })
+            //console.log(stockSymbols)
+    } catch (err) {
+        console.error("Stocks could not be loaded.")
+    }
 })
 
-app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
+/*if the socket hangs up, the server should return a 500 error  else  return the data */
+
+app.get('/stocks/:symbol', async(req, res) => {
+    try {
+        const { params: { symbol } } = req
+        const data = await stocks.getStockPoints(symbol, new Date())
+        res.send(data)
+            //console.log(data)
+    } catch (err) {
+        console.error("Stock could not be loaded.")
+        res.status(500).send(err.message)
+    }
 })
 
 app.listen(3000, () => console.log('Server is running!'))
